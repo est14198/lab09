698aeb758537df442b7579a7c84f4138fb99355f
Fernanda Estrada <est14198@uvg.edu.gt>
2019-11-15 13:33:53 -0600

    build

---
+++ .out/precache-manifest.48fdf24fb4e530fd1689a7051e136d8a.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "643b0be90ccb97fa393759042282b11f",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.77755645e4cf52fa11e2.bundle.js"
+  },
+  {
+    "revision": "207090771457f3545ec8",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.77755645e4cf52fa11e2.bundle.js"
+  },
+  {
+    "url": "vendors~main.77755645e4cf52fa11e2.bundle.js"
+  }
+]);
